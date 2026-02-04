d2b8451b3c78f6608397a751e266ba73caa38474
Samuel Warrie <104434957+samuel-warrie@users.noreply.github.com>
2026-02-04 16:24:23 +0200

    Added ChatbotMessages.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Cart from '../cart/Cart';
-import WelcomePopup from '../WelcomePopup';
+import ChatbotMessages from '../ChatbotMessages';
 import { CartProvider } from '../../context/CartContext';
 
 interface LayoutProps {
@@ -17,7 +17,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <main className="flex-grow">{children}</main>
         <Cart />
         <Footer />
-        <WelcomePopup />
+        <ChatbotMessages />
       </div>
     </CartProvider>
   );
