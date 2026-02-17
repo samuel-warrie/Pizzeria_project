88109203524539e5041bad999c6bc2defedd5876
ThanSitt <t3mith00@students.oamk.fi>
2026-02-17 19:24:22 +0200

    added mock data for testing and content based AI-recommender

--- backend/app/api/recommendations.py
+++ backend/app/api/recommendations.py
@@ -1,15 +1,18 @@
 from fastapi import APIRouter
+from app.recommender.model import get_popular_pizzas
+from app.recommender.model import get_diet_recommendations
+from app.recommender.model import recommend_for_user
 
 router = APIRouter()
 
 @router.get("/popular")
 def get_popular():
-    return {"message": "Popular recommendation endpoint working"}
+    return get_popular_pizzas()
 
 @router.get("/user/{user_id}")
 def get_user_recommendations(user_id: int):
-    return {"message": f"User recommendation endpoint working for {user_id}"}
+    return recommend_for_user(user_id)
 
 @router.get("/diet/{diet_type}")
-def get_diet_recommendations(diet_type: str):
-    return {"message": f"Diet recommendation endpoint working for {diet_type}"}
+def get_diet(diet_type: str):
+    return get_diet_recommendations(diet_type)
---
+++ backend/app/recommender/model.py
@@ -0,0 +1,61 @@
+from .data import load_data
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+orders, menu = load_data()
+
+menu["features"] = (
+    menu["ingredients"] + " " +
+    menu["category"] + " " +
+    menu["diet"]
+)
+
+def get_popular_pizzas(top_n=3):
+    popular = orders['pizza_name'].value_counts().head(top_n)
+    return popular.index.tolist()
+
+def get_diet_recommendations(diet_type, top_n=3):
+    filtered = menu[menu["diet"] == diet_type]
+
+    #if no matches found
+    if filtered.empty:
+        return {"error": "No pizzas found for this diet type"}
+
+    #return random selection of pizzas from the filtered list
+    return filtered["name"].sample(min(top_n, len(filtered))).tolist()
+
+vectorizer = TfidfVectorizer()
+feature_matrix = vectorizer.fit_transform(menu["features"])
+similarity_matrix = cosine_similarity(feature_matrix)
+
+def recommend_similar_pizzas(pizza_name, top_n=3):
+    #find index of pizza
+    idx = menu[menu["name"] == pizza_name].index
+
+    if len(idx) == 0:
+        return {"error": "Pizza not found"}
+
+    idx = idx[0]
+
+    #get similarity scores
+    scores = list(enumerate(similarity_matrix[idx]))
+
+    #sort by similarity
+    scores = sorted(scores, key=lambda x: x[1], reverse=True)
+
+    #skip first one since it's the same pizza
+    similar_indices = [i[0] for i in scores[1:top_n+1]]
+
+    return menu.iloc[similar_indices]["name"].tolist()
+
+def recommend_for_user(user_id):
+
+    user_orders = orders[orders["user_id"] == user_id]
+
+    if user_orders.empty:
+        return {"message": "No orders found"}
+
+    last_pizza = user_orders.iloc[-1]["pizza_name"]
+
+    return recommend_similar_pizzas(last_pizza)
+
